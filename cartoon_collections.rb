1e0229423755ef559db160806748c680dcd13b75
George  Pianka <gpianka1@gmail.com>
2018-04-23 12:39:02 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,3 +1,4 @@
+require 'pry'
 def roll_call_dwarves(array)
   array.each_with_index {|name, i| puts "#{i + 1} #{name}"}
 end
@@ -9,7 +10,7 @@ end
 def long_planeteer_calls(array)
   array.any? {|string| string.length > 4}
 end
-  
+  binding.pry
 
 def find_the_cheese# code an argument here
   # the array below is here to help
