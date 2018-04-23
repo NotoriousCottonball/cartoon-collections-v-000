373d0d86b23ca25ae44e6df61bfd87591e6da31a
George  Pianka <gpianka1@gmail.com>
2018-04-23 12:40:58 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ end
 def long_planeteer_calls(array)
   array.any? {|string| string.length > 4}
 end
-  binding.pry
+ 
 
 def find_the_cheese# code an argument here
   # the array below is here to help
