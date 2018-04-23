38202b72a7905418c018d4cc7211d61596674639
George  Pianka <gpianka1@gmail.com>
2018-04-23 12:52:00 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,9 +11,9 @@ def long_planeteer_calls(array)
 end
  
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  
+  array.detect {|
 
 end
 
